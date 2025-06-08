8e25c8e854777b344a0a988529d378638229d14d
sanjivthapa <thapasvt12@gmail.com>
2025-06-08 10:57:44 +0545

    some more feature added using oauth2 to get user youtube info too soon implementing it

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -30,8 +30,17 @@ GOOGLE_REDIRECT_URI = os.getenv("REDIRECT_URI")
 #this get method returns a url when you goto url it redirects to google login page
 @router.get("/login/google")
 async def login_google():
+    url = (
+    f"https://accounts.google.com/o/oauth2/auth?"
+    f"response_type=code&"
+    f"client_id={GOOGLE_CLIENT_ID}&"
+    f"redirect_uri={GOOGLE_REDIRECT_URI}&"
+    f"scope=openid%20email%20profile%20https://www.googleapis.com/auth/youtube.readonly&"
+    f"access_type=offline&"
+    f"prompt=consent"
+)
     return{
-        "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
+        "url": url
     }
 
 #this should be same url as in google cloud console authorized redirect URIs
@@ -49,9 +58,21 @@ async def auth_google(code: str):
     }
     response = requests.post(token_url, data=data)
     access_token = response.json().get("access_token")
-    refresh_token = response.json().get("refresh_token")
-    user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
-    return user_info.json()
+    
+    user_info_resp = requests.get(
+        "https://www.googleapis.com/oauth2/v1/userinfo",
+        headers={"Authorization": f"Bearer {access_token}"})
+    user_info = user_info_resp.json()
+
+    youtube_info_resp = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true",
+        headers={"Authorization": f"Bearer {access_token}"})
+    youtube_data = youtube_info_resp.json()
+    
+    return {
+        "profile": user_info,
+        "youtube": youtube_data,
+    }
 
 @router.get("/token")
 async def get_token(token: str = Depends(oauth2_scheme)):
